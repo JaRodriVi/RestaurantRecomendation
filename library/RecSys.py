308c268d25fd7d589a22c446379255e8d7130091
JaRodriVi <jarodrivi@gmacil.com>
2018-11-09 18:39:09 +0100

    library

--- library/RecSys.py
+++ library/RecSys.py
@@ -18,7 +18,26 @@ import seaborn as sns
 ### Replace the falseID for the realID on data[ID]
 # -----------
 def replaceID(realID, falseID, ID, data):
-    data = list(data)
     for df in data:
-        if ID in list(df.columns):
-            df[ID] = df[ID].replace(falseID,realID)        
+        if ID in list(list(df)):
+            df[ID] = df[ID].replace(falseID,realID)     
+            
+# -----------
+### params: realID::String
+# data:: single DataFrame or a list of DataFrame
+# find:: String
+# -----------
+### Find in data the "find" parametter, and return for 
+### each data a list of columns that have "find"
+# -----------
+def checkData(data, find):
+    dataFind = []
+    indexAndColumn = []
+    for i in range(len(data)):
+        df = data[i]
+        for col in list(df):
+            if (len(df[df[col].astype(str)==find])!=0):
+                indexAndColumn.append((i,col))
+                dataFind.append(df[df[col]==find])
+    return indexAndColumn,dataFind 
+    
